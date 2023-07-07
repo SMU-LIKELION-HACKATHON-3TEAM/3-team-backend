6fb50c8b7f4dd87a3005f00873b63f4fcb2d6134
DDonghyeo <gsu9045000@gmail.com>
2023-07-07 21:59:36 +0900

    Feat: Item Entity

--- src/main/java/com/grishare/domain/Item.java
+++ src/main/java/com/grishare/domain/Item.java
@@ -1,9 +1,18 @@
 package com.grishare.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Table(name = "Item")
 @Entity
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int item_id;
+
+    @Column(name = "classification_id")
+    private int classification_id;
+
+    @Column(name = "ItemName")
+    private char itemName;
 }
