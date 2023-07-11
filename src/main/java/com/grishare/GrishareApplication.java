81a99b13d4da3be1666449c5468a21600e4c8bae
guitarcode <choicco89@gmail.com>
2023-07-11 22:17:13 +0900

    fix: CORS 오류 수정을 위한 코드 추가

--- src/main/java/com/grishare/GrishareApplication.java
+++ src/main/java/com/grishare/GrishareApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 public class GrishareApplication {
 
