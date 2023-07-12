f4a4f404ba9f1d1e85f72b5afa60810b27ba4323
DDonghyeo <gsu9045000@gmail.com>
2023-07-12 14:02:06 +0900

    Fix: Quote Nation Return Dto

--- src/main/java/com/grishare/service/QuoteServiceImpl.java
+++ src/main/java/com/grishare/service/QuoteServiceImpl.java
@@ -4,17 +4,21 @@ import com.grishare.base.BaseResponse;
 import com.grishare.domain.AdministrativeDivision;
 import com.grishare.domain.Nation;
 import com.grishare.domain.Quote;
+import com.grishare.dto.QuoteNationReturnDto;
 import com.grishare.exception.CustomException;
 import com.grishare.exception.ErrorCode;
 import com.grishare.repository.AdministrativeDivisionRepository;
 import com.grishare.repository.NationRepository;
 import com.grishare.repository.QuoteRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class QuoteServiceImpl implements QuoteService {
 
@@ -29,7 +33,12 @@ public class QuoteServiceImpl implements QuoteService {
 
     public BaseResponse<?> getCountry() {
         List<Nation> nationList = nationRepository.findAll();
-        return BaseResponse.ok(nationList);
+        log.info("nation List 불러오기 완료");
+        List<QuoteNationReturnDto> quoteNationReturnDtoList = nationList.stream()
+                .map(nation -> new QuoteNationReturnDto(nation.getIso2(), nation.getCountryName()))
+                .toList();
+        log.info("dto 변환 완료");
+        return BaseResponse.ok(quoteNationReturnDtoList);
     }
 
     public BaseResponse<?> getDivision(char iso) {
