bd6f1560169402a0fb74843c0cb762d83c11b37a
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2023-07-13 19:38:40 +0900

    Refactor : User 정보 변경 수정

--- src/main/java/com/grishare/dto/UserRequestDto.java
+++ src/main/java/com/grishare/dto/UserRequestDto.java
@@ -11,14 +11,16 @@ public class UserRequestDto {
     // 회원 수정 관련 requestDto -> 비밀번호, 닉네임, 사진을 수정할 수 있다!
     private String password;
     private String nickName;
-    private String picture;
+    private String userImg;
     private String userLoginId;
+    private String backgroundImg;
 
     public UserRequestDto(User user) {
         this.password = password;
         this.nickName = nickName;
-        this.picture = picture;
+        this.userImg = userImg;
         this.userLoginId = userLoginId;
+        this.backgroundImg = backgroundImg;
     }
 }
 
