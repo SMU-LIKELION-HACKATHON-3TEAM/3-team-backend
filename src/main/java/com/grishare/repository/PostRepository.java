ff2835924326c56c04e2ed22a172d3b83de510a4
noeyoes <lsy021128@naver.com>
2023-07-14 03:48:31 +0900

    Feat: 게시글 최신 게시글 순서로 정렬 기능 구현

--- src/main/java/com/grishare/repository/PostRepository.java
+++ src/main/java/com/grishare/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.grishare.repository;
 import com.grishare.domain.Post;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.Entity;
@@ -12,6 +13,7 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     @EntityGraph(attributePaths = {"user", "postImage"})
+    @Query("select p from Post as p order by p.createdAt DESC") // 게시글 최신 순서로 정렬하는 query문
     List<Post> findAll();
     public List<Post> findAllByNationId(long nationId);
 
