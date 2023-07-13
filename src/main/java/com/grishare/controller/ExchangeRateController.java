51e4c28fc28185666bc76fb0ceafc3fb739452ac
Eunji1217 <94281256+Eunji1217@users.noreply.github.com>
2023-07-13 20:56:03 +0900

    Refactor : Controller 수정

--- src/main/java/com/grishare/controller/ExchangeRateController.java
+++ src/main/java/com/grishare/controller/ExchangeRateController.java
@@ -1,5 +1,6 @@
 package com.grishare.controller;
 
+import com.grishare.base.BaseResponse;
 import com.grishare.dto.ExchangeRateReturnDto;
 import com.grishare.service.ExchangeRateServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +17,14 @@ public class ExchangeRateController {
     ExchangeRateServiceImpl exService;
 
     @GetMapping("/exchangeRate/{contryName}/{bank}")
-    public ResponseEntity<ExchangeRateReturnDto> getExchangeRate(@PathVariable("contryName") String contryName, @PathVariable("bank") String bank){
+    public BaseResponse<ExchangeRateReturnDto> getExchangeRate(@PathVariable("contryName") String contryName, @PathVariable("bank") String bank){
         ExchangeRateReturnDto exReturnDto = exService.findByContryNameAndBank(contryName, bank);
-        return ResponseEntity.ok(exReturnDto);
+        return BaseResponse.ok(exReturnDto);
     }
 
     @GetMapping("/exchangeRate")
-    public ResponseEntity<List<ExchangeRateReturnDto>> getExchangeRates(){
+    public BaseResponse<List<ExchangeRateReturnDto>> getExchangeRates(){
         List<ExchangeRateReturnDto> exReturnDtoList = exService.findAll();
-        return ResponseEntity.ok(exReturnDtoList);
+        return BaseResponse.ok(exReturnDtoList);
     }
 }
