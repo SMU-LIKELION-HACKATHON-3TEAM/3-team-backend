224a0db199b827a43c59e31e352ec70c4d077dc6
rlagns1234 <tonyk0521@naver.com>
2023-07-13 16:29:05 +0900

    Fix: 신고 게시글 유저 인식

--- src/main/java/com/grishare/service/UpdateService.java
+++ src/main/java/com/grishare/service/UpdateService.java
@@ -1,11 +1,13 @@
 package com.grishare.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CurrencyEditor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.Currency;
+import java.util.Locale;
 
 @Service
 public class UpdateService {
@@ -14,10 +16,12 @@ public class UpdateService {
     ExchangeRateServiceImpl exService;
 
     @Scheduled(cron = "0 1 9-22 * * 1-5", zone = "Asia/Seoul")
-//    @Scheduled(cron = "0 * * * * 1-5", zone = "Asia/Seoul")
+//    @Scheduled(cron = "* * * * * 1-5", zone = "Asia/Seoul")
     public void updateExchangeRate() throws IOException {
         exService.update();
-//        Currency c = new Currency
+//        Locale locale1 = new Locale("en", "US");
+//        String s = Currency.getInstance(Locale.CANADA).getSymbol();
+//        System.out.println(s);
     }
 
 }
