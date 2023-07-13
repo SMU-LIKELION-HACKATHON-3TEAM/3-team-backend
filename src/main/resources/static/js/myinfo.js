7dd49c52f7e7c4a6462f723d5231ec244d2059d2
wooren <127368021+wooren@users.noreply.github.com>
2023-07-14 08:41:18 +0900

    메인페이지 내정보 수정

--- src/main/resources/static/js/myinfo.js
+++ src/main/resources/static/js/myinfo.js
@@ -1,16 +1,16 @@
 $.ajax({
     type: 'GET',
     url: 'http://grishare.ap-northeast-2.elasticbeanstalk.com/api/mypage',
-    success: function() {
-        $.each(data.data, function (index, item) {
+    success: function(data) {
+        
 
-            var $username = $('<div>').addClass('username').text(item.username)
-            var $email = $('<div>').addClass('email').text(item.email)
-            var $picture = $('<div>').addClass('picture').text(item.picture)
-            var $nickname = $('<div>').addClass('nickname').text(item.nickname)
+            var $username = $('<div>').addClass('username').text(data.data.username)
+            var $email = $('<div>').addClass('email').text(data.data.email)
+            var $picture = $('<div>').addClass('picture').text(data.data.picture)
+            var $nickname = $('<div>').addClass('nickname').text(data.data.nickname)
 
             $('#wrap-myid').append($username).append($email).append($picture).append($nickname)
-        });
+        
     },
     error: function() {
         console.log("실패");
