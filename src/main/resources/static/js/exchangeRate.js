f3628d2b0e3d4d4c9d4b349543913dc5c65c2ed0
leeeemj <leemj2944@gmail.com>
2023-07-14 09:03:20 +0900

    feat:다시 수정

--- src/main/resources/static/js/exchangeRate.js
+++ src/main/resources/static/js/exchangeRate.js
@@ -81,7 +81,7 @@ $(document).ready(function () {
                         "\n" +
                         "error:" +
                         error
-                    );
+                    // );
                 }
             });
 
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -29,7 +29,7 @@ function onSubmitLogin(){
           "\n" +
           "error:" +
           error
-      );
+      // );
     },
   });
 }
